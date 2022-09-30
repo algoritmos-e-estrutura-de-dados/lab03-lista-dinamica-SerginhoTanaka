85ee4dfbd80d6724e403bb21c83f298597a275e1
Sergio Kenji Sawasaki Tanaka <serginho.tanaka@edu.unifil.br>
2022-09-30 18:05:18 -0300

    Atividade Lista dinamica

---
+++ aula19-09/lista.py
@@ -0,0 +1,52 @@
+from node import Node
+
+
+class Lista:
+    inicio = None
+
+    def __init__(self):
+        self.inicio = None
+
+        # adicionar no final da lista
+
+    def adicionar(self, valor, inicio=False):
+        if inicio:
+            self.adicionar_no_inicio(valor)
+        else:
+            self.adicionar_no_fim(valor)
+
+    def adicionar_no_inicio(self, valor):
+        novo = Node(valor)
+        novo.proximo = self.inicio
+        self.inicio = novo
+
+    def adicionar_no_fim(self, valor):
+        if self.inicio is None:
+            self.inicio = Node(valor, None)
+        else:
+            aux = self.inicio
+            while aux.proximo is not None:
+                aux = aux.proximo
+
+            aux.proximo = Node(valor, None)
+            aux.proximo.anterior = aux
+
+    def remover(self, valor):
+        aux = self.inicio
+        if aux.valor == valor:
+            # aux.valor = None
+            self.inicio = aux.proximo
+        else:
+            while aux.proximo is not None:
+                aux = aux.proximo
+                if aux.valor == valor:
+                    aux.anterior.proximo = aux.proximo
+
+    # lista dinamica
+    def show(self):
+        aux = self.inicio
+        print("[", end='')
+        while aux.proximo is not None:
+            print(f"{aux.valor}", end=', ')
+            aux = aux.proximo
+        print(f"{aux.valor}]")
